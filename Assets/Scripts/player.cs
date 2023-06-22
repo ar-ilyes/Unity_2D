3ec0d1103200fb14651bf059fe84d165c5194ee6
ilyes <armilyes@gmail.com>
2023-06-22 23:50:52 +0200

    adding player movement

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -8,6 +8,17 @@ public class player : MonoBehaviour
     public float moveForce = 10f;
     [SerializeField]
     public float jumpForce = 11f;
+    private float movementX;
+    private Rigidbody2D myBody;
+    private SpriteRenderer sr;
+    private Animator anim;
+    private string WALK_ANIMATION = "isWalking";
+
+    private void Awake(){
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +28,22 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerWalk();
+        AnimatePlayer();
+    }
+    void PlayerWalk(){
+        movementX = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(movementX , 0f , 0f) *Time.deltaTime * moveForce ;
+    }
+    void AnimatePlayer(){
+        if(movementX>0){
+            anim.SetBool(WALK_ANIMATION,true);
+            sr.flipX=false;
+        }else if(movementX<0){
+            anim.SetBool(WALK_ANIMATION,true);
+            sr.flipX=true;
+        }else{
+            anim.SetBool(WALK_ANIMATION,false);
+        }
     }
 }
