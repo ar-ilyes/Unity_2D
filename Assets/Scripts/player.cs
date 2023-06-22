7b7456c9dbff4c4a11286ecb6cdff1092620fab0
ilyes <armilyes@gmail.com>
2023-06-22 23:27:32 +0200

    adding player animations

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    [SerializeField]
+    public float moveForce = 10f;
+    [SerializeField]
+    public float jumpForce = 11f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
